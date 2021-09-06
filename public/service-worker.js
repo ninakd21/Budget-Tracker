0f64438816535a348e83a515cb1cc8c4f14a534c
Nina Davis <nina.k.davis2@gmail>
2021-09-06 14:09:28 -0500

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,20 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-  "./index.html",
-  "./public/css/style.css",
-  "./dist/app.bundle.js",
-  "./dist/events.bundle.js",
-  "./dist/tickets.bundle.js",
-  "./dist/schedule.bundle.js"
+  '/',
+  '/index.html',
+  '/js/index.js',
+  '/js/idb.js',
+  '/manifest.json',
+  '/css/styles.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 // Respond with cached resources
